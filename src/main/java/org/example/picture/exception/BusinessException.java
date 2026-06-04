a597e86089e4ea0768332658ac8366b4720b71e0
ssz159 <moyuba_159@qq.com>
2026-06-04 19:51:29 +0800

    基础代码

---
+++ src/main/java/org/example/picture/exception/BusinessException.java
@@ -0,0 +1,30 @@
+package org.example.picture.exception;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
+@Data
+public class BusinessException extends RuntimeException {
+
+    /**
+     * 错误码
+     */
+    private final int code;
+
+    public BusinessException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+    }
+
+}
