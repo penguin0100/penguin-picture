137156a6017b59a49097e4b2e5334de3e15d9a54
ssz159 <moyuba_159@qq.com>
2026-06-04 19:41:32 +0800

    初始化项目

---
+++ src/main/java/org/example/picture/PictureApplication.java
@@ -0,0 +1,13 @@
+package org.example.picture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureApplication.class, args);
+    }
+
+}
